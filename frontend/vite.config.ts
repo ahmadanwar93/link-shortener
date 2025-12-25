76ee07e5e56b68967de58b5d472efbd760d9771c
ahmadanwar93 <ahmadfikri.anwarfadzil@gmail.com>
2025-12-25 16:35:10 +0800

    feat: env config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
+// controls how Vite builds and serves the app
 export default defineConfig({
-  plugins: [react()],
-})
+  // transform JSX, process CSS
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // Runtime: import "@/foo" → "./src/foo"
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
